1df10d20b658d94488d3afd984c84e4048b7e268
Hyunseok Byun <gumyoincirno@gmail.com>
2024-06-20 00:29:29 +0900

    Fix database specs for longtext

--- api-server/models.py
+++ api-server/models.py
@@ -9,8 +9,8 @@ class DisasterMessage(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     create_date = Column(DateTime, index=True)
-    location_id = Column(String(100), index=True)
-    location_name = Column(String(255))
+    location_id = Column(String(5000), index=True)  # Updated length to 5000
+    location_name = Column(String(10000))  # Updated length to 10000
     md101_sn = Column(String(50), unique=True, index=True)
     msg = Column(String(1000))
     send_platform = Column(String(50))
