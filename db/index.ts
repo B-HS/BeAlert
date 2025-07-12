5b2b1efd0d4a14bb590f6bb005ef8e93eae9b9fa
Hyunseok Byun <gumyoincirno@gmail.com>
2025-07-13 00:34:07 +0900

    remove migrate when production

--- db/index.ts
+++ db/index.ts
@@ -6,8 +6,4 @@ import { migrate } from 'drizzle-orm/bun-sqlite/migrator'
 const sqlite = new Database('db/sqlite.db')
 export const db = drizzle(sqlite)
 
-if (Bun.env.NODE_ENV !== 'production') {
-    migrate(db, { migrationsFolder: './db/drizzle' })
-}
-
 export { schema }
